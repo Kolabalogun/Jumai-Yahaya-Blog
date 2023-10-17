b226d5c1e09139f2b7d19e579835ef68ed1beb5a
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-10-17 21:52:29 +0100

    Update Firebase.js

--- src/Utils/Firebase.js
+++ src/Utils/Firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNpRc39SdR8xHmJeJHv3-vrQVJkhZl2_0",
-  authDomain: "yahayajumai.firebaseapp.com",
-  databaseURL: "https://yahayajumai-default-rtdb.firebaseio.com",
-  projectId: "yahayajumai",
-  storageBucket: "yahayajumai.appspot.com",
-  messagingSenderId: "252669103951",
-  appId: "1:252669103951:web:40c208526cbdc2ff52ff3a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
